934b210ac9c955dc4e0f0c5635df6912974fd9d5
Nishant Kumar Behl <Nishant.Behl@oneadvanced.com>
2021-10-17 13:29:49 +0530

    with Hashset

--- FAANG/TwoSum.java
+++ FAANG/TwoSum.java
@@ -1,4 +1,5 @@
 import java.lang.module.FindException;
+import java.util.HashSet;
 
 
 
@@ -11,7 +12,8 @@ public class TwoSum {
     // OPTIMIZED solution : use hashmap to store the number of find which is hte difference of current number ad target, iterate through the list\array, store the NumberToFind(Target-current)
     // into hashmap,move on to next number and see if number to find exist in hashmap, if not , move on to next number, if found ,return the value of hashmap(which in this case is index) & current index
 
-    public static int[] findNumber(int[] arr, int targetVar){
+    /* BRUTE FORCE FUNCTION
+    public static int[] findSumOfTwo(int[] arr, int targetVar){
 
         int[] output = {0,0};
             for (int i=0;i< arr.length;i++){
@@ -29,13 +31,37 @@ public class TwoSum {
 
         return output;
     }
+*/
 
+    //OPTIMIZED SOLUTION USING HASHMAP
+    // #1 - return the numbers using HashSets
+
+    public static int[] findSumOfTwo(int[] arr, int target) {
+        var output = new int[]{0,0};
+        
+        HashSet<Integer> hs = new HashSet<>();
+        
+        for (int i = 0;i< arr.length;i++){
+   
+            int numberToFind = target - arr[i];
+
+             if (hs.contains(numberToFind)){
+                output[0] = arr[i];
+                output[1] = numberToFind;
+                return output;
+        }
+            hs.add(arr[i]);
+         }   
+        return output;
+    }
+
+    
 
 public static void main(String[] args) {
     int targetVar = 10;
     int arr[] = {1,3,7,9,2};
     
-    int prt[] = TwoSum.findNumber(arr, targetVar);
+    int prt[] = TwoSum.findSumOfTwo(arr, targetVar);
     System.out.print("two indices are found and their value is : " + prt[0] + " & "  + prt[1] );
 }
 }
